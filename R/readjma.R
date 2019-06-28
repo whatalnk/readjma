e175bb285a9b2764e6d6e254ed0671f63eea3a7f
Wataru NAKAI <watnk0806@gmail.com>
2019-06-28 14:02:53 +0900

    init

---
+++ R/readjma.R
@@ -0,0 +1,9 @@
+#' readjma
+#'
+#' read climate data from Japan Meteorological Agency web site
+#'
+#' @name readjma
+#' @docType package
+#' @import purrr stringr lubridate httr xml2 selectr
+#' @importFrom magrittr "%>%"
+NULL
