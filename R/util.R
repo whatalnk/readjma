3024d0166446a31b385bb8ab295f822df41cd32a
Wataru NAKAI <watnk0806@gmail.com>
2019-06-28 20:29:17 +0900

    Add util

---
+++ R/util.R
@@ -0,0 +1,22 @@
+#' split interval by 1 month
+#'
+split_interval <- function(from, to){
+  d1 <- floor_date(from, unit="month")
+  d2 <- ceiling_date(to, unit="month")
+  x <- seq.Date(d1, d2, "1 month")
+  ret <- seq_len(length(x) - 1) %>%
+    purrr::map(function(i){
+      interval(x[i], x[i+1])
+    })
+  ret
+}
+
+#' wait for given time
+#' @param sec time in second
+wait <- function(sec){
+  pb <- progress_bar$new(format = "(:spin) waiting [:bar] :elapsed", total = sec + 1, clear = FALSE, show_after = 0)
+  for (i in 1:(sec + 1)) {
+    pb$tick()
+    Sys.sleep(1)
+  }
+}
